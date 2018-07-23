b2aad551f81a1a71c2aa22a60126a7187333a2cc
roca <romulo.cintra@gft.com>
2018-07-23 23:54:16 +0100

    amend projectname

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -32,7 +32,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'o-rango-platform',
+  projectName: 'o-rango.github.io',
   organizationName: 'o-rango',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
